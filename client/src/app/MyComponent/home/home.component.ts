9efb46b5ace44198bc72b515c96b1e60d441a58c
AlgorithmChopda <vinaychopda4@gmail.com>
2023-05-17 08:52:39 +0530

    feat: added the navigation to the search button

--- client/src/app/MyComponent/home/home.component.ts
+++ client/src/app/MyComponent/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(public router: Router) {}
+}
